d115e7f5f89490c03288e385cefe0d165c3dc17b
alazzuri <alexislazzuri@gmail.com>
2021-06-14 21:21:57 -0300

    Minor fixes

--- README.md
+++ README.md
@@ -1,17 +1,16 @@
-# Ejercicio de Practica. ✏️
+# Ejercicio de Práctica. ✏️
 
-En el presente ejercicio, partiremos de un formulario de login previamente implementado, en donde la persona debera ingresar su email y contrasena.
+En el presente ejercicio, partiremos de un formulario de login previamente implementado, en donde la persona debera ingresar su email y contraseña.
 
-La consigna del ejercicio, consiste en simular una validacion asincronica, que se haria comparando la informacion ingresada por la persona, contra los datos que se encuentran almacenados en el servidor.
+La consigna del ejercicio, consiste en simular una validación asincrónica, la que en la vida real se realizaría comparando la información ingresada por la persona, contra los datos que se encuentran almacenados en el servidor.
 
-Para ello, simularemos un tiempo de respuesta (hasta que se resuelva la peticion al servidor), de tres segundos. Durante dicho lapso, deberemos indicar al usuario que
-la aplicacion se encuentre "Iniciando Sesion..."
+Para ello, simularemos un tiempo de respuesta (hasta que se resuelva la peticion al servidor), de tres segundos. Durante dicho lapso, deberemos indicar al usuario que la aplicacion se encuentra "Iniciando Sesión".
 
-Asimismo, a partir de los datos ingresados, validaremos si los mismos coinciden con la informacion del usuario almacenada en la base de datos. En caso de que los mismos no coincidan, deberemos mostrar un mensaje de error.
+Asimismo, a partir de los datos ingresados, validaremos si los mismos cumplen con ciertos requisitos pre-establecidos y, además, si coinciden con la información del usuario almacenada en la base de datos. En caso de que los mismos no coincidan, deberemos mostrar un mensaje de error.
 
-Si los datos ingresados son correctos, se debera mostrar un mensaje de bienvenida al usuario.
+Si los datos ingresados son correctos, se deberá mostrar un mensaje de bienvenida la persona.
 
-Para resolver esta actividad, deberas examinar y prestar atencion a los distintos tags html y sus distintas propiedades, para ver de que manera puedes seleccionar cada elemento mediante javascript. 👀 👀 👀
+Para resolver esta actividad, deberás examinar y prestar atención a los distintos tags html y sus distintas propiedades, para ver de que manera puedes seleccionar y manipular cada elemento mediante javascript. De la misma manera, en el archivo styles.css, te dejamos algunos estilos que podrán serte de utilidad para realizar la tarea asignada. 👀 👀 👀
 
 ## Ejemplo:
 
--- scripts/index.js
+++ scripts/index.js
@@ -32,59 +32,39 @@ const baseDeDatos = {
 
 // Paso a paso:
 
-// 1) Escuchar el evento necesario para reaccionar cuando el usuario
-// haga click en el boton iniciar sesion.
+// 1) Escuchar el evento necesario para reaccionar cuando la persona
+// haga click en el botón iniciar sesión.
 
-// 2) El proceso de inicio de sesion debera tener una demora de 3 segundos.
-// Deberas agregar la funcion correspondiente para simular dicha demora.
+// 2) El proceso de inicio de sesión deberá tener una demora de 3 segundos.
+// Deberás agregar la función correspondiente para simular dicha demora.
 
-// 3) Durante el tiempo indicado anteriormente, se debera mostrar el mensaje "Iniciando sesion..."
+// 3) Durante el tiempo indicado anteriormente, se deberá mostrar el mensaje "Iniciando sesión..."
 
 // 4) A partir de los inputs ingresados en el formulario, se deberan realizar las siguientes validaciones:
-// 1) Que el primer input sea un email valido.
-// 2) Que la contrasena tenga al menos 5 caracteres.
+// 1) Que el primer input sea un email válido.
+// 2) Que la contraseña tenga al menos 5 caracteres.
 // 3) Que los datos ingresados corresponden a una
 // persona que se encuentre registrada en la base de datos.
 // En caso de que alguna de las validaciones no sea exitosa,
-// se debera mostrar un mensaje de error que diga "Alguno de los datos ingresados son incorrectos"
+// se deberá mostrar un mensaje de error que diga "Alguno de los datos ingresados son incorrectos"
 
-// 5) En caso de que los datos ingresados sean correctos, se debera ocultar el formulario y mostrar un mensaje de
-// bienvenida al usuario.
+// 5) En caso de que los datos ingresados sean correctos, se deberá ocultar el formulario y mostrar
+// un mensaje de bienvenida al sitio.
 
-const loginBtn = document.querySelector(".login-btn");
-const errorContainer = document.querySelector("#error-container");
-const loader = document.querySelector("#loader");
+/* 
+TIPS:
+  - Puedes averiguar acerca de la manera de validar el formato de un email utilizando Javascript, buscando
+    en internet frases como "Validar email con Javascript o similar".
 
-function validarUsuario() {
-  const email = document.querySelector("#email-input").value;
-  const password = document.querySelector("#password-input").value;
-  const errorMsj = `<small>Alguno de los datos ingresados son incorrectos</small>`;
-  const main = document.querySelector("main");
+  - Recuerda que puedes seleccionar y manipular los elementos del archivo index.html, usando los
+    recursos que Javascript te ofrece para ello. Además, en el archivo styles.css tiene algunas clases y 
+    estilos predefinidos para ayudarte a completar la actividad.
 
-  const usuarioExistente = baseDeDatos.usuarios.find(
-    (usuario) => usuario.email === email
-  );
+  - También te dejamos algunos mensajes que te pueden ser de utilidad:
+  
+   Mensaje de error => <small>Alguno de los datos ingresados son incorrectos</small>
 
-  loader.classList.add("hidden");
+   Mensaje de bienvenida => "<h1> Bienvenido al sitio 😀 </h1>";
 
-  if (!usuarioExistente) {
-    errorContainer.innerHTML = errorMsj;
-    errorContainer.classList.remove("hidden");
-    return;
-  }
-
-  if (usuarioExistente.password === password) {
-    main.innerHTML = "<h1> Bienvenido al sitio 😀 </h1>";
-  } else {
-    errorContainer.innerHTML = errorMsj;
-    errorContainer.classList.remove("hidden");
-    return;
-  }
-}
-loginBtn.addEventListener("click", () => {
-  loader.classList.remove("hidden");
-  errorContainer.classList.add("hidden");
-  setTimeout(() => {
-    validarUsuario();
-  }, 3000);
-});
+   ¡Manos a la obra!
+ */
